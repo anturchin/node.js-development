f1895b895d52fd8e43bdb92b7370653f4f0b8814
anturchin <tur4intwo@gmail.com>
2024-05-04 21:59:24 +0300

    feat: implement task print-tree

---
+++ print-tree/tree.js
@@ -0,0 +1,64 @@
+exports.tree = [
+    {
+        "name": 1,
+        "items": [
+            {
+                "name": 2,
+                "items": [
+                    { "name": 3 },
+                    { "name": 4 },
+                ]
+            },
+            {
+                "name": 5,
+                "items": [
+                    { "name": 6 },
+                    { "name": 7 },
+                ]
+            },
+            {
+                "name": 8,
+                "items": [
+                    { "name": 9 },
+                ]
+            }
+        ]
+    },
+    {
+        "name": 10,
+        "items": [
+            {
+                "name": 11,
+                "items": [
+                    { "name": 12 },
+                    { "name": 13 },
+                ]
+            },
+            {
+                "name": 14,
+                "items": [
+                    { "name": 15 },
+                    {
+                        "name": 16,
+                        "items": [
+                            {
+                                "name": 17,
+                                "items": [
+                                    { "name": 18 },
+                                    { "name": 19 },
+                                ]
+                            },
+                            {
+                                "name": 20,
+                                "items": [
+                                    { "name": 21 },
+                                ]
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    },
+    { "name": 22 },
+];
