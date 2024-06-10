9b06fd6f15300dee7f02336e981dcb1368d7ba35
anturchin <tur4intwo@gmail.com>
2024-06-10 15:07:53 +0300

    feat: add comment and rating service

--- courses-app-ts/api/src/services/commentService/CommentService.ts
+++ courses-app-ts/api/src/services/commentService/CommentService.ts
@@ -1,1 +1,20 @@
-export class CommentService {}
+import { IComment } from '../../models/comment/types';
+import Comment from '../../models/comment/Comment';
+
+export class CommentService {
+    public async createNewComment(commentData: IComment): Promise<IComment> {
+        const newComment = new Comment(commentData);
+        await newComment.save();
+        return newComment;
+    }
+
+    public async findCommentsByCourseId(courseId: string): Promise<IComment[]> {
+        const foundComments = await Comment.find({ courseId }).exec();
+        return foundComments;
+    }
+
+    public async deleteComment(commentId: string): Promise<IComment | null> {
+        const deletedComment = await Comment.findByIdAndDelete(commentId).exec();
+        return deletedComment;
+    }
+}
--- courses-app-ts/api/src/services/ratingService/RatingService.ts
+++ courses-app-ts/api/src/services/ratingService/RatingService.ts
@@ -1,1 +1,20 @@
-export class RatingService {}
+import Rating from '../../models/rating/Rating';
+import { IRating } from '../../models/rating/types';
+
+export class RatingService {
+    public async createNewRating(rating: IRating): Promise<IRating> {
+        const newRating = new Rating(rating);
+        await newRating.save();
+        return newRating;
+    }
+
+    public async findRatingsByCourseId(courseId: string): Promise<IRating[]> {
+        const foundRatings = await Rating.find({ courseId }).exec();
+        return foundRatings;
+    }
+
+    public async deleteRating(ratingId: string): Promise<IRating | null> {
+        const deletedRating = await Rating.findByIdAndDelete(ratingId).exec();
+        return deletedRating;
+    }
+}
