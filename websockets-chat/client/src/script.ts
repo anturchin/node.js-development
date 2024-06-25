0d57b0a7187f761ea0af8a9034959874dd37aa02
anturchin <tur4intwo@gmail.com>
2024-06-25 21:41:04 +0300

    feat: implement web worker

--- websockets-chat/client/src/script.ts
+++ websockets-chat/client/src/script.ts
@@ -1,39 +1,73 @@
-document.addEventListener('DOMContentLoaded', () => {
-    
-    const chatWindow = document.getElementById('chat-window') as HTMLElement;
-    const chatInput = document.getElementById('chat-input') as HTMLInputElement;
-    const sendButton = document.getElementById('send-button') as HTMLButtonElement;
+class Chat {
 
-    const ws = new WebSocket('ws://localhost:8080');
+    private readonly chatWindow: HTMLElement;
+    private readonly chatInput: HTMLInputElement;
+    private readonly sendButton: HTMLButtonElement;
+    private readonly worker: Worker;
 
-    ws.onmessage = (event) => {
-        const message = event.data;
-        addMessageToChatWindow(message);
-    };
+    constructor() {
+        this.chatWindow = document.getElementById('chat-window') as HTMLElement;
+        this.chatInput = document.getElementById('chat-input') as HTMLInputElement;
+        this.sendButton = document.getElementById('send-button') as HTMLButtonElement;
+        this.worker = new Worker('./dist/worker.js');
+        this.worker.postMessage('ws://localhost:8080'); 
+        this.onMessage();
+        this.setEventListener();
+    }
 
-    sendButton?.addEventListener('click', () => {
-        sendMessage();
-    });
+    private onMessage(): void {
+        this.worker.onmessage = (event: MessageEvent) => {
+            const message = event.data;
+            this.addMessageToChatWindow(message);
+            this.showNotification('Новое сообщение', message);
+        };
+    }
 
-    chatInput?.addEventListener('keypress', (event) => {
-        if (event.key === 'Enter') {
-            sendMessage();
-        }
-    });
+    private setEventListener(): void {
 
-    function sendMessage() {
-        const message = chatInput.value.trim();
+        this.sendButton.addEventListener('click', () => {
+            this.sendMessage();
+        });
+
+        this.chatInput.addEventListener('keypress', (event: KeyboardEvent) => {
+            if (event.key === 'Enter') {
+                this.sendMessage();
+            }
+        });
+    }
+
+    private sendMessage(): void {
+        const message = this.chatInput.value.trim();
         if (message) {
-            ws.send(message);
-            addMessageToChatWindow(`You: ${message}`);
-            chatInput.value = '';
+            this.worker.postMessage({ type: 'send', message: message });
+            this.addMessageToChatWindow(`( you ): ${message}`, true);
+            this.chatInput.value = '';
         }
     }
 
-    function addMessageToChatWindow(message: string) {
-        const messageElement = document.createElement('p');
-        messageElement.textContent = message;
-        chatWindow.appendChild(messageElement);
-        chatWindow.scrollTop = chatWindow.scrollHeight;
+    private showNotification(title: string, message: string): void {
+        if (Notification.permission === 'granted') {
+            new Notification(title, { body: message });
+        } else if (Notification.permission !== 'denied') {
+            Notification.requestPermission().then(permission => {
+                if (permission === 'granted') {
+                    new Notification(title, { body: message });
+                }
+            });
+        }
     }
-});
+
+    private addMessageToChatWindow(message: string, myMessage: boolean = false): void {
+        const messageElement = document.createElement('p');
+        if (myMessage) {
+            messageElement.classList.add('left');
+        } else {
+            messageElement.classList.add('right');
+        }
+        messageElement.textContent = message;
+        this.chatWindow.appendChild(messageElement);
+        this.chatWindow.scrollTop = this.chatWindow.scrollHeight;
+    }
+}
+
+new Chat();
